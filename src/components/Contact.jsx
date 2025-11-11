856223bd41d932e0da79b5d58f7995e0f5c02541
Alakok77 <top.p1234558@gmail.com>
2025-11-11 11:05:57 +0700

    finish mobile site

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,39 +13,41 @@ export default function Contact(){
     };
 
     return (
-        <div className="h-80 mt-10 flex justify-center" ref={contactRef}>
-            <div className="shadow-[0_0_10px_5px_rgba(0,0,0,0.1)] w-250 h-70 rounded-2xl p-5 flex flex-col gap-5 justify-center items-center">
-                <h1 className="font-bold text-cyan-800 text-2xl ml-10 mr-20">Contact : </h1>
-                <div className='flex gap-5 justify-center'>
-                    <img className="w-15 h-15 rounded-lg shadow-[0_0_5px_2px_rgba(0,0,0,0.1)] 
+        <div className="mt-10 flex justify-center p-10" ref={contactRef}>
+            <div className="shadow-[0_0_10px_5px_rgba(0,0,0,0.1)] w-250 rounded-2xl p-5
+                            flex flex-col gap-2 justify-center items-center">
+                <h1 className="font-bold text-cyan-800 text-2xl ml-10 mr-20">Contact</h1>
+                <div className='flex gap-5 justify-center
+                                flex-wrap'>
+                    <img className="md:w-15 md:h-15 w-10 h-10 rounded-lg shadow-[0_0_5px_2px_rgba(0,0,0,0.1)] 
                                     flex flex-col justify-center cursor-pointer
                                     transition-transform duration-300 hover:scale-105 hover:shadow-[0_0_10px_3px_rgba(0,0,0,0.15)]
                                     "
                         onClick={() => {openlink("https://mail.google.com/mail/?view=cm&fs=1&to=top.p1234558@gmail.com")}}
                         src={gmail}>
                     </img>
-                    <img className="w-15 h-15 rounded-2xl shadow-[0_0_5px_2px_rgba(0,0,0,0.1)] 
+                    <img className="md:w-15 md:h-15 w-10 h-10 rounded-2xl shadow-[0_0_5px_2px_rgba(0,0,0,0.1)] 
                                         flex flex-col justify-center cursor-pointer
                                         transition-transform duration-300 hover:scale-105 hover:shadow-[0_0_10px_3px_rgba(0,0,0,0.15)]
                                         "
                         onClick={() => {openlink("https://line.me/ti/p/~0825508806")}}
                         src={line}>
                     </img>
-                    <img className="w-15 h-15 rounded-4xl shadow-[0_0_5px_2px_rgba(0,0,0,0.1)] 
+                    <img className="md:w-15 md:h-15 w-10 h-10 rounded-4xl shadow-[0_0_5px_2px_rgba(0,0,0,0.1)] 
                                         flex flex-col justify-center cursor-pointer
                                         transition-transform duration-300 hover:scale-105 hover:shadow-[0_0_10px_3px_rgba(0,0,0,0.15)]
                                         "
                         onClick={() => {openlink("https://www.instagram.com/to.exphp/")}}
                         src={ig}>
                     </img>
-                    <img className="w-15 h-15 rounded-lg shadow-[0_0_5px_2px_rgba(0,0,0,0.1)] 
+                    <img className="md:w-15 md:h-15 w-10 h-10 rounded-lg shadow-[0_0_5px_2px_rgba(0,0,0,0.1)] 
                                         flex flex-col justify-center cursor-pointer
                                         transition-transform duration-300 hover:scale-105 hover:shadow-[0_0_10px_3px_rgba(0,0,0,0.15)]
                                         "
                         onClick={() => {openlink("https://web.facebook.com/phiphop.thong.aun?locale=th_TH")}}
                         src={facebook}>
                     </img>
-                    <img className="w-15 h-15 rounded-full shadow-[0_0_5px_2px_rgba(0,0,0,0.1)] 
+                    <img className="md:w-15 md:h-15 w-10 h-10 rounded-full shadow-[0_0_5px_2px_rgba(0,0,0,0.1)] 
                                         flex flex-col justify-center cursor-pointer
                                         transition-transform duration-300 hover:scale-105 hover:shadow-[0_0_10px_3px_rgba(0,0,0,0.15)]
                                         "
@@ -53,8 +55,8 @@ export default function Contact(){
                         src={github}>
                     </img>
                 </div>
-                <h1 className="font-bold text-cyan-800 text-lg ml-10 mr-20">Location : <span className='font-medium text-cyan-700 text-base'>Base in Bangkok, Thailand</span></h1>        
-                <h1 className="font-bold text-cyan-800 text-lg ml-10 mr-20">Available hours : <span className='font-medium text-cyan-700 text-base'>9 AM – 6 PM</span></h1>        
+                <h1 className="font-bold text-cyan-800 md:text-lg ml-10 mr-20 text-sm">Location : <span className='font-medium text-cyan-700 md:text-base text-sm'>Base in Bangkok, Thailand</span></h1>        
+                <h1 className="font-bold text-cyan-800 md:text-lg ml-10 mr-20 text-sm">Available hours : <span className='font-medium text-cyan-700 md:text-base text-sm'>9 AM – 6 PM</span></h1>        
             </div>
         </div>
     )
